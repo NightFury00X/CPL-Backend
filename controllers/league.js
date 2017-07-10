3e37ffdc60d0478e02c701b567b1ec385db19bad
[NightFury00X] <[surendra136721@gmail.com]>
2017-07-10 21:42:25 +0530

    League change

--- controllers/league.js
+++ controllers/league.js
@@ -7,36 +7,36 @@ const User = require('../models/user');
 exports.create = function (req, res, next) {
     // Check for registration errors
     const leagueName = req.body.leagueName;
-    const user = User.findById(req.body.user);
-    console.log("User: ", user);
+
     // Return error if no league name provided
     if (!leagueName) {
         return res.status(422).send({error: 'You must enter a valid league name.'});
     }
-    
+
     League.findOne({leagueName}, (err, existingLeague) => {
         if (err) {
             return next(err);
         }
-        
+
         // If league is not unique, return error
         if (existingLeague) {
             return res.status(422).send({error: 'That league name is already in use.'});
         }
         // If league name is unique create league
-        const league = new League({
-            leagueName,
-            user
-        });
-        
-        
+        const league = new League();
+        league.leagueName = leagueName;
+
+        //find user
+        league.user = req.body.user;  //User.findById(req.body.user);
+
+
         league.save((err, league) => {
             if (err) {
                 return next(err);
             }
-            
+
             //const userInfo = setUserInfo(user);
-            
+
             res.status(200).json({
                 token: 'token',
                 league: league
